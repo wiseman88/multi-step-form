1c66225492e5d73358d03bd908b98e85fe06272a
wiseman88 <wiseadam02@gmail.com>
2022-12-21 21:41:30 +0100

    refactor: display components via loop

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,13 @@ import Addons from './components/Addons.vue';
 import Summary from './components/Summary.vue';
 import Confirmation from './components/Confirmation.vue';
 
+const components = [Personal, Plans, Addons, Summary, Confirmation]
+
 </script>
 
 <template>
   <main>
-    <Personal />
-    <Plans />
-    <Addons />
-    <Summary />
-    <Confirmation />
+    <component v-for="(component, index) in components" :is="component" :key="index" :id="index"></component>
   </main>
 </template>
 
