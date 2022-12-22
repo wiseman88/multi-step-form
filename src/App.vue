cf75720be5809a0be17e0be9493c0f5a678b2719
wiseman88 <wiseadam02@gmail.com>
2022-12-22 17:27:30 +0100

    feat: next/prev step

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,15 @@ import { ref } from 'vue';
 const components = [Personal, Plans, Addons, Summary, Confirmation]
 
 let currentStep = ref(0)
+let steps = components.length
+
+const nextStep = () => {
+  currentStep.value++
+}
+
+const prevStep = () => {
+  currentStep.value--
+}
 
 </script>
 
@@ -16,10 +25,10 @@ let currentStep = ref(0)
   <main>
     <component v-for="(component, index) in components" :is="component" :key="index" :id="index"
       :currentStep="currentStep"></component>
-    <div>
-      <button>go back</button>
-      <button>next step</button>
-      <button>confirm</button>
+    <div v-if="currentStep <= (steps - 2)">
+      <button v-if="currentStep !== 0" @click="prevStep">go back</button>
+      <button v-if="currentStep !== (steps - 2)" @click="nextStep">next step</button>
+      <button v-if="(currentStep + 1) === (steps - 1)" @click="nextStep">confirm</button>
     </div>
   </main>
 </template>
